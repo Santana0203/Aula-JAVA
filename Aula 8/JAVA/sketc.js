f9b809a8afb992f562e6e15261d14887871875a7
Santana0203 <140172279+Santana0203@users.noreply.github.com>
2023-08-04 11:46:40 -0300

    Add files via upload

---
+++ Aula 8/JAVA/sketc.js
@@ -0,0 +1,23 @@
+//Cdg de alerta confirmação e prompt simples
+//window.alert('Meu  primeiro Alerta')
+//window.confirm('Está gostando do curso')
+//window.prompt('Qual o seu nome?')
+
+//variaveis
+//var nome = window.prompt('Qual o seu nome?')
+//window.alert('Seja bem vindo ' + nome + '!')
+
+/*Criando 2 numeros
+var n1 = Number.parseInt(window.prompt('Digite um número'))
+var n2 = Number.parseInt(window.prompt('Digite outro número'))
+var s = n1 + n2
+var s = window.alert('A soma dos números é ' + s)*/
+
+//JavaScript decide se é inteiro ou flutuante
+var n1 = Number(window.prompt('Digite um número'))
+var n2 = Number(window.prompt('Digite outro número'))
+var s = n1 + n2
+var s = window.alert('A soma dos números é ' + s)
+
+
+
